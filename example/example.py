bbcf752abb35ac5814ff51af01d03638bf764c85
William <xf0718@gmail.com>
2016-05-16 11:29:37 +0800

    add example

---
+++ example/example.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from sqlalchemy import *
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+metadata = MetaData()
+users = Table('users', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('name', String),
+        Column('fullname', String),
+)
+addresses = Table('addresses', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('user_id', None, ForeignKey('users.id')),
+        Column('email_address', String, nullable=False),
+)
+
+metadata.create_all(engine)
+
+ins = users.insert().values(name='Jack', fullname='Jack Jones')
+
+conn = engine.connect()
+result = conn.execute(ins)
+
+ins = users.insert()
+conn.execute(ins, id=2, name='Wendy', fullname='Wendy Williams')
+
+conn.execute(addresses.insert(), [ 
+        {'user_id': 1, 'email_address' : 'jack@yahoo.com'},
+        {'user_id': 1, 'email_address' : 'jack@msn.com'},
+        {'user_id': 2, 'email_address' : 'www@www.org'},
+        {'user_id': 2, 'email_address' : 'wendy@aol.com'},
+])
+
+s = select([users])
+result = conn.execute(s)
